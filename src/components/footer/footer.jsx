e624bb41a9e511425960e5116cf44e4692bce96d
tjlin653 <tonylin415@gmail.com>
2026-04-20 18:05:36 -0700

    navbar buttons

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -2,7 +2,7 @@ import styles from "./footer.module.scss";
 
 export default function Footer() {
   return (
-    <div> Footer yay</div>
+    <div></div>
 
   );
 }
--- src/components/navBar/navBar.jsx
+++ src/components/navBar/navBar.jsx
@@ -2,7 +2,15 @@ import styles from "./navBar.module.scss";
 
 export default function Navbar() {
   return (
-    <div> Navbar yay</div>
-
+    <div className={styles.navShell}>
+      <img src="/Dark_Blue_Logo.svg" alt="JCC logo" />
+      <div className={styles.navButtons}>
+        <button className={styles.homeBtn}>Home</button>
+        <button className={styles.aboutBtn}>About</button>
+        <button className={styles.committeesBtn}>Committees</button>
+        <button className={styles.contactBtn}>Contact Us</button>
+        <button className={styles.joinBtn}>Join Us</button>
+      </div>
+    </div>
   );
 }
