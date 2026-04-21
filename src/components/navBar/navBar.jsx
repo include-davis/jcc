893750d66b764c226ce094490b1ba4b9ca051177
tjlin653 <tonylin415@gmail.com>
2026-04-20 18:53:14 -0700

    committees dropdown menu

--- src/components/navBar/navBar.jsx
+++ src/components/navBar/navBar.jsx
@@ -7,7 +7,17 @@ export default function Navbar() {
       <div className={styles.navButtons}>
         <button className={styles.homeBtn}>Home</button>
         <button className={styles.aboutBtn}>About</button>
-        <button className={styles.committeesBtn}>Committees</button>
+        <div className={styles.committeeDropdown}>
+          <button className={styles.committeesBtn}>Committees</button>
+          <img className={styles.dropdownArrow} src="/dropdown_arrow.svg" alt="dropdown arrow"/>
+          <div className={styles.dropdownMenu}>
+            <a href="#">Dental Committee</a>
+            <a href="#">Mental Health & Wellness Committee</a>
+            <a href="#">Physical Integrated Health Committee</a>
+            <a href="#">Community Outreach Committee</a>
+            <a href="#">Sexual & Reproductive Committee</a>
+          </div>
+        </div>
         <button className={styles.contactBtn}>Contact Us</button>
         <button className={styles.joinBtn}>Join Us</button>
       </div>
