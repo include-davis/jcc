7d9c2fd5323ca0c0a2a7df692ddd99b18103a403
Yash Pradhan <yspradhan@ucdavis.edu>
2026-04-22 01:32:31 -0700

    top and bottom aesthetics fixed as of April 21st designs

--- src/pages/committees/index.jsx
+++ src/pages/committees/index.jsx
@@ -5,22 +5,25 @@ export default function Committees() {
   return (
     <div className={styles.container}>
       Committees
+      
       <div className={styles.committees_top}>
-        <div className={styles.committees_top_inside}>
-          <div className={styles.committees_top_content}>
-            <div className={styles.committees_top_content_left}>
-              <h1>JCC Committees</h1>
-              <p>Our clinic consists of five distinct committees, each specializing in a different area of health. Each team collaborates together to bring support and care for the Davis community through its wellness initiatives. </p>
-              <div className={styles.buttons}>
-                <button className={styles.button}>Committees</button>
-                <button className={styles.button}>Collaboration</button>
-              </div>
+        <div className={styles.committees_top_content}>
+          <div className={styles.committees_top_content_left}>
+            <h1>JCC Committees</h1>
+            <p>Our clinic consists of five distinct committees, each specializing in a different area of health. Each team collaborates together to bring support and care for the Davis community through its wellness initiatives.</p>
+            <div className={styles.buttons}>
+              <button className={styles.button}>Committees</button>
+              <button className={styles.button}>Collaboration</button>
             </div>
-            <div className={styles.committees_top_content_right_logo_frame}>
-              <img src="public/Dark_Blue_Logo.png" alt="JCC Logo"/>
-            </div>
-          </div> 
-        </div>
+          </div>
+          <div className={styles.committees_top_content_right_logo_frame}>
+            <img src="/Dark_Blue_Logo.png" alt="JCC Logo"/>
+          </div>
+        </div> 
+      </div>
+
+      <div className={styles.committees_bottom}>
+        {/* Here will be committee cards map */}
       </div>
 
 
