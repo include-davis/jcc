6d098f08282f5ce2459f96d30d2660bd5f793771
Yash Pradhan <yspradhan@ucdavis.edu>
2026-04-22 15:11:52 -0700

    correction - these are not buttons, they are static tags

--- src/pages/committees/index.jsx
+++ src/pages/committees/index.jsx
@@ -12,8 +12,8 @@ export default function Committees() {
             <h1>JCC Committees</h1>
             <p>Our clinic consists of five distinct committees, each specializing in a different area of health. Each team collaborates together to bring support and care for the Davis community through its wellness initiatives.</p>
             <div className={styles.buttons}>
-              <button className={styles.button}>Committees</button>
-              <button className={styles.button}>Collaboration</button>
+              <div className={styles.button}>Committees</div>
+              <div className={styles.button}>Collaboration</div>
             </div>
           </div>
           <div className={styles.committees_top_content_right_logo_frame}>
