4192c1c8774e4ef8430d22bd9c1599cab5a48d85
Yash Pradhan <yspradhan@ucdavis.edu>
2026-04-21 18:58:27 -0700

    structure for committees top section added, no styling as of yet

--- src/pages/committees/index.jsx
+++ src/pages/committees/index.jsx
@@ -5,6 +5,27 @@ export default function Committees() {
   return (
     <div className={styles.container}>
       Committees
+      <div className={styles.committees_top}>
+        <div className={styles.committees_top_inside}>
+          <div className={styles.committees_top_content}>
+            <div className={styles.committees_top_content_left}>
+              <h1>JCC Committees</h1>
+              <p>Our clinic consists of five distinct committees, each specializing in a different area of health. Each team collaborates together to bring support and care for the Davis community through its wellness initiatives. </p>
+              <div className={styles.buttons}>
+                <button className={styles.button}>Committees</button>
+                <button className={styles.button}>Collaboration</button>
+              </div>
+            </div>
+            <div className={styles.committees_top_content_right}>
+              <div className={styles.committees_top_content_logo_frame}>
+                <img src="public/Dark_Blue_Logo.png" alt="JCC Logo"/>
+              </div>
+            </div>
+          </div> 
+        </div>
+      </div>
+
+
     </div>
   );
 }
