935cacd2c929c4e9ebd72a5631a9d9dfe6e7a428
jordanjt4 <jordan.jt4@comcast.net>
2026-04-27 15:05:36 -0700

    increased gap between three components in first row, added console logs, changed routes

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -38,52 +38,76 @@ export default function Footer() {
 
         <div className={styles.home}> 
           <h4>Home</h4>
-          <Link href="/" className={styles.linkReset}>
+          <Link href="/" 
+            className={styles.linkReset}
+            onClick={() => console.log("Home section clicked")}>
             <p>Section</p>
           </Link>
-          <Link href="/" className={styles.linkReset}>
+          <Link href="/e" 
+            className={styles.linkReset}
+            onClick={() => console.log("Home section clicked")}>
             <p>Section</p>
           </Link>
-          <Link href="/" className={styles.linkReset}>
+          <Link href="/" 
+            className={styles.linkReset}
+            onClick={() => console.log("Home section clicked")}>
             <p>Section</p>
           </Link>
         </div>
 
         <div className={styles.committees}>
-          <h4>Commitees</h4>
-          <Link href="/" className={styles.linkReset}>
+          <h4>Committees</h4>
+          <Link href="/committees" 
+            className={styles.linkReset}
+            onClick={() => console.log("Committees section clicked")}>
             <p>Section</p>
           </Link>
-          <Link href="/" className={styles.linkReset}>
+          <Link href="/committees" 
+            className={styles.linkReset}
+            onClick={() => console.log("Committees section clicked")}>
             <p>Section</p>
           </Link>
-          <Link href="/" className={styles.linkReset}>
+          <Link href="/committees" 
+            className={styles.linkReset}
+            onClick={() => console.log("Committees section clicked")}>
             <p>Section</p>
           </Link>
         </div>
 
         <div className={styles.history}>
           <h4>History</h4>
-          <Link href="/" className={styles.linkReset}>
+          <Link href="/timeline" 
+            className={styles.linkReset}
+            onClick={() => console.log("History section clicked")}>
             <p>Section</p>
           </Link>
-          <Link href="/" className={styles.linkReset}>
+          <Link href="/timeline" 
+            className={styles.linkReset}
+            onClick={() => console.log("History section clicked")}>
             <p>Section</p>
           </Link>
-          <Link href="/" className={styles.linkReset}>
+          <Link href="/timeline" 
+            className={styles.linkReset}
+            onClick={() => console.log("History section clicked")}>
             <p>Section</p>
             </Link>
         </div>
 
         <div className={styles.contact}>
           <h4>Contact</h4>
-          <Link href="/" className={styles.linkReset}>
+          <Link href="/contact" 
+            className={styles.linkReset}
+            onClick={() => console.log("Contact section clicked")}>
             <p>Section</p>
           </Link>
-          <Link href="/" className={styles.linkReset}>
+          <Link href="/contact" 
+            className={styles.linkReset}
+            onClick={() => console.log("Contact section clicked")}>
             <p>Section</p>
           </Link>
-          <Link href="/" className={styles.linkReset}>
+          <Link href="/contact" 
+            className={styles.linkReset}
+            onClick={() => console.log("Contact section clicked")}>
             <p>Section</p>
           </Link>
         </div>
@@ -92,10 +116,14 @@ export default function Footer() {
       <div className={styles.copyright}>
         <h4>© Juvenile Caduceus Clinic</h4>
         <div className={styles.socials}>
-          <Link href="/" className={styles.linkReset}>
+          <Link href="https://www.instagram.com/jcclinic/" 
+            className={styles.linkReset}
+            onClick={() => console.log("Instagram clicked")}>
             <FaInstagram style={{ fontSize: "30px"}} />
           </Link>
-          <Link href="/" className={styles.linkReset}>
+          <Link href="/contact" 
+            className={styles.linkReset}
+            onClick={() => console.log("Email clicked")}>
             <MdEmail style={{ fontSize: "30px" }} />
           </Link>
         </div>
