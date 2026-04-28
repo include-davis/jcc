f7b32c538234cd21f4962186d8a1ae115f76a93c
Yash Pradhan <yspradhan@ucdavis.edu>
2026-04-27 18:01:44 -0700

    minor link fix

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -43,7 +43,7 @@ export default function Footer() {
             onClick={() => console.log("Home section clicked")}>
             <p>Section</p>
           </Link>
-          <Link href="/e" 
+          <Link href="/" 
             className={styles.linkReset}
             onClick={() => console.log("Home section clicked")}>
             <p>Section</p>
