2d0b558b494f0b777802df841d7af3a2539edb29
tjlin653 <tonylin415@gmail.com>
2026-04-27 18:23:11 -0700

    about dropdown + all dropdown links

--- src/components/navBar/navBar.jsx
+++ src/components/navBar/navBar.jsx
@@ -6,16 +6,24 @@ export default function Navbar() {
       <img src="/Dark_Blue_Logo.svg" alt="JCC logo" />
       <div className={styles.navButtons}>
         <button className={styles.homeBtn}>Home</button>
-        <button className={styles.aboutBtn}>About</button>
+        <div className={styles.aboutDropdown}>
+          <button className={styles.aboutBtn}>About</button>
+          <img className={styles.dropdownArrow} src="/dropdown_arrow.svg" alt="dropdown arrow"/>
+          <div className={styles.aboutDropdownMenu}>
+            <a href="/history">History</a>
+            <a href="/partnerships">Partnership</a>
+            <a href="/alumni">Alumni</a>
+          </div>
+        </div>
         <div className={styles.committeeDropdown}>
           <button className={styles.committeesBtn}>Committees</button>
           <img className={styles.dropdownArrow} src="/dropdown_arrow.svg" alt="dropdown arrow"/>
-          <div className={styles.dropdownMenu}>
-            <a href="#">Dental Committee</a>
-            <a href="#">Mental Health & Wellness Committee</a>
-            <a href="#">Physical Integrated Health Committee</a>
-            <a href="#">Community Outreach Committee</a>
-            <a href="#">Sexual & Reproductive Committee</a>
+          <div className={styles.committeeDropdownMenu}>
+            <a href="/committees_dental">Dental Committee</a>
+            <a href="/committees_mental">Mental Health & Wellness Committee</a>
+            <a href="/committees_physical">Physical Integrated Health Committee</a>
+            <a href="/committees_community">Community Outreach Committee</a>
+            <a href="/committees_sexual">Sexual & Reproductive Committee</a>
           </div>
         </div>
         <button className={styles.contactBtn}>Contact Us</button>
