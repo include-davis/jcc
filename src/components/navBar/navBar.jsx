be32523dcb7278d3c49c5df3ea4dbb5563cb1c40
tjlin653 <tonylin415@gmail.com>
2026-04-28 00:22:10 -0700

    changed home page link

--- src/components/navBar/navBar.jsx
+++ src/components/navBar/navBar.jsx
@@ -23,7 +23,7 @@ export default function Navbar() {
     <div className={styles.navShell} ref={navRef}>
       <img src="/Dark_Blue_Logo.svg" alt="JCC logo" />
       <div className={styles.navButtons}>
-        <a href="/home" className = {styles.homeBtn}>Home</a>
+        <a href="/" className = {styles.homeBtn}>Home</a>
         <div className={`${styles.aboutDropdown} ${openMenu === "about" ? styles.active : ""}`} onClick={() => toggleMenu("about")}>
           <button className={styles.aboutBtn}>About</button>
           <img className={styles.dropdownArrow} src="/dropdown_arrow.svg" alt="dropdown arrow"/>
