1635fe94ef389e49d14e1e88c9dc2d4f4f20a24e
Aryan <ADMIN@Aryan-Kapoors-MacBook.local>
2026-04-29 12:42:51 -0700

    addressed comments from pr

--- src/pages/partnerships/index.jsx
+++ src/pages/partnerships/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from "./partnerships.module.scss";
 import { FaEye, FaUsers, FaCheckCircle } from "react-icons/fa";
+import OfferCard from "../../components/partnerships-what-we-offer-cards/partnerships-what-we-offer-cards";
 
 export default function Partnerships() {
   {/* Made an array to eventually implement the cards from the design */}
@@ -32,14 +33,14 @@ export default function Partnerships() {
       <h1 className={styles.heading}>What we offer</h1> 
       <p className={styles.subheading}>Partners gain meaningful visibility within a mission-driven community while making a
          direct impact on the health and well-being of underserved youth.</p>
-      <div className = {styles.cards}>
-        {/* used map() to make the cards */}
-      {offers.map((item) => (
-          <div key={item.title} className={styles.card}>
-            <span className={styles.icon}>{item.icon}</span>
-            <h3 className={styles.card_title}>{item.title}</h3>
-            <p className={styles.card_desc}>{item.desc}</p>
-          </div>
+         <div className={styles.cards}>
+        {offers.map((item) => (
+          <OfferCard 
+            key={item.title}
+            icon={item.icon}
+            title={item.title}
+            desc={item.desc}
+          />
         ))}
       </div>
     </div>
