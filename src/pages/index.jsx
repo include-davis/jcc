9c7d6c0449e4b93ce30a81c85feefff3680a881b
Yash Pradhan <yspradhan@ucdavis.edu>
2026-05-04 23:39:02 -0700

    added temporary link to Join Us button

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,10 +2,6 @@ import React from 'react';
 import styles from "./home.module.scss";
 
 export default function Home() {
-  const handleJoinClick = (label) => {
-    console.log(`${label} clicked`);
-  };
-
   return (
     <div className={styles.container}>
       {/* TODO: top-page section - Refer to Figma file for
@@ -27,14 +23,9 @@ export default function Home() {
           <p className={styles.learnMoreText}>
             Want to learn more about our clinic and how to get involved? Click here to learn more about our recruitment process.
           </p>
-
-          <button
-            type="button"
-            className={styles.secondaryButton}
-            onClick={() => handleJoinClick('Join button')}
-          >
-            Join Us
-          </button>
+          <a href="/contact" className={styles.secondaryButton}>
+            Join Us 
+          </a>
         </div>
       </div>
     </div>
