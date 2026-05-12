1825e22c5598c58655fcc1c9f20b51a202d9b801
tjlin653 <tonylin415@gmail.com>
2026-05-12 16:18:38 -0700

    committee cards size increased to fit the viewport

--- src/components/committees-general-cards/data/committeesData.jsx
+++ src/components/committees-general-cards/data/committeesData.jsx
@@ -2,31 +2,31 @@ export const committeesData = [
     {
         id: 1,
         name: "Dental Health Committee",
-        icon: "/dental_health_logo.png",
+        icon: "/dental_health_logo_whitebg.svg",
         link: "/committees/dental",
     },
     {
         id: 2,
         name: "Mental Health & Wellness Committee",
-        icon: "/mental_health_and_wellness_logo.png",
+        icon: "/mental_health_logo_whitebg.svg",
         link: "/committees/mental",
     },
     {
         id: 3,
         name: "Physical & Integrated Health Committee",
-        icon: "/physical_and_integrated_health_logo.png",
+        icon: "/physical_health_logo_whitebg.svg",
         link: "/committees/physical",
     },
     {
         id: 4,
         name: "Community Outreach Committee",
-        icon: "/community_outreach_logo.png",
+        icon: "/community_outreach_logo_whitebg.svg",
         link: "/committees/community",
     },
     {
         id: 5,
         name: "Sexual & Reproductive Health Committee",
-        icon: "/sexual_and_reproductive_health_logo.png",
+        icon: "/sexual_health_logo_whitebg.svg",
         link: "/committees/sexual",
     }
 ];
