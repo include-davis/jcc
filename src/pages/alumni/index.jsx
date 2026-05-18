10b60ac957c7776adbe8c05228a520e01cb9a21e
Dylan Hoang <dylanhoang55@gmail.com>
2026-05-18 15:33:53 -0700

    added duplicate card from home page, set up skeleton

--- src/pages/alumni/index.jsx
+++ src/pages/alumni/index.jsx
@@ -3,8 +3,36 @@ import styles from "./alumni.module.scss";
 
 export default function Alumni() {
   return (
-    <div className={styles.container}>
-      Alumni
+    <div className={styles.alumniContainer}>
+      <div className={styles.topAlumniSection}>
+        {/*
+          Top section of alumni page
+        */}
+
+
+      </div>
+      <div className={styles.updateAlumniSection}>
+        {/*
+          Update card section
+        */}
+        <div className={styles.learnMoreCard}>
+          <h2 className={styles.learnMoreTitle}>Recruitment</h2>
+
+          <p className={styles.learnMoreText}>
+            Want to learn more about our clinic and how to get involved? Click here to learn more about our recruitment process.
+          </p>
+          <a href="mailto:email@example.com" className={styles.secondaryButton}>
+            Join Us
+          </a>
+        </div>
+
+      </div>
+      <div className={styles.bottomAlumniSection}> 
+        {/*
+          Share your JCC Experience section 
+        */}
+        
+      </div>
     </div>
   );
 }
