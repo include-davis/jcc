78a714c082a89794e270b8961242da68d7cc513a
Yash Pradhan <yspradhan@ucdavis.edu>
2026-05-18 19:10:02 -0700

    fixed mismatched code chunks on home page

--- src/pages/contact/index.jsx
+++ src/pages/contact/index.jsx
@@ -5,16 +5,16 @@ export default function Contact() {
     <div className={styles.div}>
       Contact
     
-    <div className={styles.mapContainer}>
-    <iframe
-      className={styles.mapFrame}
-      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6176.567440825006!2d-121.75263835211008!3d38.54389305473589!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80852909a76deced%3A0xbdeb9ff130b7a9d0!2sMemorial%20Union%20%26%20Main%20Island!5e0!3m2!1sen!2sus!4v1778546594433!5m2!1sen!2sus" 
-      style={{ border:0 }} allowfullscreen loading="lazy" 
-      referrerPolicy="no-referrer-when-downgrade"
-      title="Location map"
-    ></iframe>
-  </div>
-  </div>
+      <div className={styles.mapContainer}>
+        <iframe
+          className={styles.mapFrame}
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6176.567440825006!2d-121.75263835211008!3d38.54389305473589!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80852909a76deced%3A0xbdeb9ff130b7a9d0!2sMemorial%20Union%20%26%20Main%20Island!5e0!3m2!1sen!2sus!4v1778546594433!5m2!1sen!2sus" 
+          style={{ border:0 }} allowfullscreen loading="lazy" 
+          referrerPolicy="no-referrer-when-downgrade"
+          title="Location map"
+        ></iframe>
+      </div>
+    </div>
   );
 }
 
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import Link from "next/link";
 import styles from "./home.module.scss";
-import { useState } from 'react';
 import { FaArrowRight } from "react-icons/fa";
 
 export default function Home() {
@@ -58,68 +57,6 @@ export default function Home() {
     },
   ];
 
-  return (
-    <main className={styles.container}>
-      <section className={styles.sectionTop}>
-        top-page
-      </section>
-
-      <section className={styles.sectionMiddle}>
-        <section className={styles.OurMissionContainer}>
-          <article className={styles.OurMissionLeftContent}>
-            <h2 className={styles.MissionTitle}>Our Mission</h2>
-
-            <p className={styles.MissionText}>
-              <span className={styles.BoldIntro}>
-                In Juvenile Caduceus Clinic,
-              </span>{" "}
-              we aim to deliver high-quality care and assistance to the
-              underprivileged youth and older adolescents who are struggling and
-              seek help. Addressing the public health issues regarding a child’s
-              well-being including but not limited to childhood obesity, physical
-              activity, cognitive health, and stress is fundamental in building
-              an environment for all children to thrive in. By integrating the
-              four pillars of leadership, humility, empathy, and responsibility,
-              our goal is to work together to provide equal care with respect to
-              social, mental, dental, and overall health to children in
-              adolescent institutions and programs.
-            </p>
-          </article>
-
-          <figure className={styles.MissionImgCircle}>
-            <img
-              className={styles.MissionImg}
-              src="/jcc_logo.png"
-              alt="JCC Logo"
-            />
-          </figure>
-        </section>
-
-        <section className={styles.OurCommitteesContainer}>
-          <header className={styles.OurCommitteesTopContent}>
-            <h2 className={styles.OurCommitteesTitle}>Our Committees</h2>
-
-            <p className={styles.OurCommitteesDescription}>
-              Our Clinic consists of 5 different committees all focused on a
-              different area of health care. Learn more about each committee by
-              clicking the links below
-            </p>
-          </header>
-
-          <section className={styles.OurCommitteesBottomContent}>
-            {committees.map((committee) => (
-              <article className={styles.CommitteeItem} key={committee.route}>
-                <img
-                  className={styles.CommitteeIcon}
-                  src={committee.image}
-                  alt="Committee logo"
-                />
-
-                <Link href={committee.route} className={styles.CommitteeButton}>
-                  <span className={styles.CommitteeButtonText}>
-                    {committee.title}
-                  </span>
-  
   const [currentSlide, setCurrentSlide] = useState(0); // using useState to move through the slides
 
   // made a slides array for content of each slide
@@ -163,61 +100,124 @@ export default function Home() {
       position: "center 70%",
     },
   ];
-  
+
   return (
-    <div className={styles.container}>
-      {/* TODO: top-page section - Refer to Figma file for
-         +design specs. Write your code here. */}
+    <main className={styles.container}>
 
-      <div className={styles.sectionTop}></div>
+      <section className={styles.sectionTop}>
         <div className={styles.img_container}> {/* Image container to write over the image */}
-        <div className={styles.dots}>  {/* made a separate div element for the dots at bottom to indiciate which slide user is on*/}
+          <div className={styles.dots}>  {/* made a separate div element for the dots at bottom to indiciate which slide user is on*/}
             {slides.map((_, index) => (
-            <button
-              key={index}
-              className={`${styles.dot} ${currentSlide === index ? styles.dot_active : ''}`}
-              onClick={() => setCurrentSlide(index)}
-              />
+              <button
+                key={index}
+                className={`${styles.dot} ${currentSlide === index ? styles.dot_active : ''}`}
+                onClick={() => setCurrentSlide(index)}
+                />
             ))}
-        </div>
-        <img src={slides[currentSlide].img} alt={slides[currentSlide].title} className={styles.image} />  {/* Uploading the background image */}
-        <img src="/gradient.svg" alt="" className={styles.gradient_overlay} />  {/* Uploading the gradient for background image */}
-        {/*  Div element for the text, different padding for slide 2 and 4 to keep up the aesthetic
-        Got the text on slide from the slides array 
-        Also added small tag buttons for the first slide
-        */}
-        <div className={styles.content_left} 
-        style={
-          currentSlide === 1 ? { paddingTop: '2rem' } : 
-          currentSlide === 3 ? { paddingTop: '3.35rem' } : 
-          {}}>
-        <p className={styles.slide_label}>{slides[currentSlide].label}</p>
-        <h1 className={styles.clinic_heading}>{slides[currentSlide].title}</h1>
-        <p className={styles.clinic_subheading}>{slides[currentSlide].subtitle}</p>
-        {slides[currentSlide].tags && (
-        <div className={styles.tags}>
-          {slides[currentSlide].tags.map((tag) => (
-          <span key={tag} className={styles.tag}>{tag}</span>
-          ))}
-          
-        </div>
-        )}
-        {/* Div element for the button with that links to different pages for more information
-        got its txt from the slides array 
-        */}
-        <a href={slides[currentSlide].btnLink} className={styles.learn_more_btn}>
-        <h6>{slides[currentSlide].btnText} <span className={styles.arrow}><FaArrowRight /></span></h6>
-        </a>
-        </div>
-        {/*Buttons for moving through the slides*/}
-        <button className={styles.arrow_left} onClick={() => setCurrentSlide((currentSlide - 1 + slides.length) % slides.length)}>
-        ‹
-        </button>
-        <button className={styles.arrow_right} onClick={() => setCurrentSlide((currentSlide + 1) % slides.length)}>
-        ›
-        </button>
-         </div>
+          </div>
 
+          <img src={slides[currentSlide].img} alt={slides[currentSlide].title} className={styles.image} />  {/* Uploading the background image */}
+          <img src="/gradient.svg" alt="" className={styles.gradient_overlay} />  {/* Uploading the gradient for background image */}
+
+          {/*  Div element for the text, different padding for slide 2 and 4 to keep up the aesthetic
+          Got the text on slide from the slides array 
+          Also added small tag buttons for the first slide
+          */}
+          <div className={styles.content_left} 
+            style={
+              currentSlide === 1 ? { paddingTop: '2rem' } : 
+              currentSlide === 3 ? { paddingTop: '3.35rem' } : 
+              {}}>
+            <p className={styles.slide_label}>{slides[currentSlide].label}</p>
+            <h1 className={styles.clinic_heading}>{slides[currentSlide].title}</h1>
+            <p className={styles.clinic_subheading}>{slides[currentSlide].subtitle}</p>
+            {slides[currentSlide].tags && (
+              <div className={styles.tags}>
+                {slides[currentSlide].tags.map((tag) => (
+                  <span key={tag} className={styles.tag}>{tag}</span>
+                ))}
+                
+              </div>
+            )}
+            {/* Div element for the button with that links to different pages for more information
+            got its txt from the slides array 
+            */}
+            <Link href={slides[currentSlide].btnLink} className={styles.learn_more_btn}>
+              <span>
+                {slides[currentSlide].btnText}
+                <span className={styles.arrow}>
+                  <FaArrowRight />
+                </span>
+              </span>
+            </Link>
+          </div>
+
+          {/*Buttons for moving through the slides*/}
+          <button className={styles.arrow_left} onClick={() => setCurrentSlide((currentSlide - 1 + slides.length) % slides.length)}>
+            ‹
+          </button>
+          <button className={styles.arrow_right} onClick={() => setCurrentSlide((currentSlide + 1) % slides.length)}>
+            ›
+          </button>
+        </div>
+      </section>
+
+      <section className={styles.sectionMiddle}>
+        <section className={styles.OurMissionContainer}>
+          <article className={styles.OurMissionLeftContent}>
+            <h2 className={styles.MissionTitle}>Our Mission</h2>
+
+            <p className={styles.MissionText}>
+              <span className={styles.BoldIntro}>
+                In Juvenile Caduceus Clinic,
+              </span>{" "}
+              we aim to deliver high-quality care and assistance to the
+              underprivileged youth and older adolescents who are struggling and
+              seek help. Addressing the public health issues regarding a child’s
+              well-being including but not limited to childhood obesity, physical
+              activity, cognitive health, and stress is fundamental in building
+              an environment for all children to thrive in. By integrating the
+              four pillars of leadership, humility, empathy, and responsibility,
+              our goal is to work together to provide equal care with respect to
+              social, mental, dental, and overall health to children in
+              adolescent institutions and programs.
+            </p>
+          </article>
+
+          <figure className={styles.MissionImgCircle}>
+            <img
+              className={styles.MissionImg}
+              src="/jcc_logo.png"
+              alt="JCC Logo"
+            />
+          </figure>
+        </section>
+
+        <section className={styles.OurCommitteesContainer}>
+          <header className={styles.OurCommitteesTopContent}>
+            <h2 className={styles.OurCommitteesTitle}>Our Committees</h2>
+
+            <p className={styles.OurCommitteesDescription}>
+              Our Clinic consists of 5 different committees all focused on a
+              different area of health care. Learn more about each committee by
+              clicking the links below
+            </p>
+          </header>
+  
+
+          <section className={styles.OurCommitteesBottomContent}>
+            {committees.map((committee) => (
+              <article className={styles.CommitteeItem} key={committee.route}>
+                <img
+                  className={styles.CommitteeIcon}
+                  src={committee.image}
+                  alt="Committee logo"
+                />
+
+                <Link href={committee.route} className={styles.CommitteeButton}>
+                  <span className={styles.CommitteeButtonText}>
+                    {committee.title}
+                  </span>
 
                   <img
                     className={styles.CommitteeArrowIcon}
@@ -231,11 +231,7 @@ export default function Home() {
           </section>
         </section>
       </section>
-
-      <section className={styles.sectionBottom}>
-        bottom-page
-      </section>
-    </main>
+    
       <div className={styles.sectionBottom}>
         <div className={styles.learnMoreCard}>
           <h2 className={styles.learnMoreTitle}>Recruitment</h2>
@@ -243,11 +239,11 @@ export default function Home() {
           <p className={styles.learnMoreText}>
             Want to learn more about our clinic and how to get involved? Click here to learn more about our recruitment process.
           </p>
-          <a href="/contact" className={styles.secondaryButton}>
+          <Link href="/contact" className={styles.secondaryButton}>
             Join Us 
-          </a>
+          </Link>
         </div>
       </div>
-    </div>
+    </main>
   );
 }
