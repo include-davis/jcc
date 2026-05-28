34899bcea88db988b12e20c9fd144e8202e09a91
tjlin653 <tonylin415@gmail.com>
2026-05-28 00:39:35 -0700

    cleaned up some unecessary lines

--- src/components/committees-general-cards/committeesGeneralCards.jsx
+++ src/components/committees-general-cards/committeesGeneralCards.jsx
@@ -12,11 +12,9 @@ export default function CommitteeCard({ name, icon, link }) {
       <div className={styles.cardBottom}>
         <h3>{name}</h3>
         
-        <Link href={link} passHref legacyBehavior>
-          <a className={styles.learnMoreBtn}>
+        <Link href={link} className={styles.learnMoreBtn}>
             <span className={styles.learnMore}>Learn More</span>
             <img src="arrow_pointing_sideways.svg" alt="Arrow Pointing Right" className={styles.arrowIcon} />
-          </a>
         </Link>
       </div>
     </div>
